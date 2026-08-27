fdb4b9dda2aa05270e5f7e33a4ee9e007264ba23
Ehab <ehab.mostafa96@hotmail.com>
2026-08-27 21:20:03 +0300

    home

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ app.use(cors());
 
 app.use('/api', todosRouter);
 
+app.use('/', (req, res) => {
+  res.send('Welcome to the ToDo API');
+});
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
